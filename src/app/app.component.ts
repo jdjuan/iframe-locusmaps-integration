853f4ed46bce0d6ee3c624be554f0ec3e5eab87b
jdjuan <david.juanherrera@gmail.com>
2018-01-26 10:27:04 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('myIframe') input: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  changeLocation() {
+    this.input.nativeElement.contentWindow.top
+      .LocusMaps({
+        command: 'setPosition',
+        lat: 9.06792587586318,
+        lng: -79.0697255561301
+      })
+      .then(x => console.log(x))
+      .catch(err => console.log(err));
+  }
+}
